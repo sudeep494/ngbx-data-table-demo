393cfa83cb40507e8e6371f626926a1539637d2b
sudeep shrestha <sudeep494@gmail.com>
2019-03-03 18:37:49 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbxDataTableModule } from 'ngbx-data-table';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		HttpClientModule,
+		NgbxDataTableModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
